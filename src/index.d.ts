d6ac26ffd666db21d538c909bdc2d64bf31696f6
PoweredByWard <warddecoster2003@gmail.com>
2022-11-28 13:24:46 +0100

    export to index.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,8 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 export function createContext(ctx: any): Promise<Context>;
 
-export * from ".prisma/client/index.d";
-
 export interface Context {
   prisma: PrismaClient;
 }
