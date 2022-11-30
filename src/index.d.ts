b5bb49bb009b29166421d9b55a7774d10516225a
PoweredByWard <warddecoster2003@gmail.com>
2022-11-30 15:30:45 +0100

    new way of exporting

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-export function createContext(ctx: any): Promise<Context>;
-
-export interface Context {
-  prisma: PrismaClient;
-}
+export function createContext(ctx: any): Promise<{
+	prisma: PrismaClient;
+}>;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import { Context } from "./index.d";
 export * from ".prisma/client/index.d";
 
 const prisma = new PrismaClient();
 
-export const createContext = async (ctx: any): Promise<Context> => {
-  // Skip if you are not using a serverless environment
-  ctx.callbackWaitsForEmptyEventLoop = false;
+export const createContext = async (
+	ctx: any
+): Promise<{
+	prisma: PrismaClient;
+}> => {
+	// Skip if you are not using a serverless environment
+	ctx.callbackWaitsForEmptyEventLoop = false;
 
-  return { ...ctx, prisma };
+	return { ...ctx, prisma };
 };
